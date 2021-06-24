2096af5d92b4469d93ae05661aa6aa2abe649369
Servgit <servandolopez20@gmail.com>
2021-06-24 03:03:05 -0500

    Update MicrosoftProductSteps.java

--- Challenge1/src/test/java/stepDefinitions/MicrosoftProductSteps.java
+++ Challenge1/src/test/java/stepDefinitions/MicrosoftProductSteps.java
@@ -1,14 +1,16 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.*;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.TestProperties;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
